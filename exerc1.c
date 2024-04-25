3e4702ff8e8706d09f51333a79f64ea4e08652ee
Paulo Henrique <ph.costaalves@gmail.com>
2024-04-25 20:30:58 -0300

    melhorias do exercício 1

--- exerc1.c
+++ exerc1.c
@@ -6,15 +6,36 @@
 
 int main (void){
 
-  int n;
-  char num_invert[20];
+  int n, invert, i;
+  char numInvert[60];
+  int multi = 10;
 
-  printf("Digite um número: ");
+  printf("Digite um numero: ");
   scanf("%i", &n);
  
-  sprintf(num_invert, "%i", n);
+  invert = n % 10;
+  printf("%i\n", invert);
+  invert = n % 100;
+  printf("%i\n", invert);
+  invert = n % 1000;
+  printf("%i\n", invert);
+  invert = n % 10000;
+  printf("%i\n", invert);
+  invert = n % 100000;
+  printf("%i\n", invert);
+  
+  for (i = 1; i <= 5; i++){
+	if (i == 1){
+		invert = n % multi;
+		sprintf(numInvert, "%i", invert);
+	}else{
+		invert = n % (multi * 10);
+		sprintf(numInvert, "%i", invert);
+		multi *= 10;
+	}
+  }
 
-  printf("O número invertido é: %s", num_invert);
+  printf("%s", numInvert);
 
   return 0;
 }
