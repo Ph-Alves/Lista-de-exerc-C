55a58448bdfcbfd3426f3c8cf8ce2995576d04bc
Paulo Henrique <ph.costaalves@gmail.com>
2024-04-26 19:32:31 -0300

    Finalização do exerc1

--- exerc1.c
+++ exerc1.c
@@ -7,35 +7,16 @@
 int main (void){
 
   int n, invert, i;
-  char numInvert[60];
-  int multi = 10;
 
   printf("Digite um numero: ");
   scanf("%i", &n);
- 
-  invert = n % 10;
-  printf("%i\n", invert);
-  invert = n % 100;
-  printf("%i\n", invert);
-  invert = n % 1000;
-  printf("%i\n", invert);
-  invert = n % 10000;
-  printf("%i\n", invert);
-  invert = n % 100000;
-  printf("%i\n", invert);
   
-  for (i = 1; i <= 5; i++){
-	if (i == 1){
-		invert = n % multi;
-		sprintf(numInvert, "%i", invert);
-	}else{
-		invert = n % (multi * 10);
-		sprintf(numInvert, "%i", invert);
-		multi *= 10;
-	}
+  printf("Numero invertido: ");
+  for (i = 0; i < 5; i++){
+	invert = n % 10;
+	printf("%i", invert);
+	n /= 10;
   }
-
-  printf("%s", numInvert);
 
   return 0;
 }
