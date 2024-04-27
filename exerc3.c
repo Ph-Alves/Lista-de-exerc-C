27c4cd7e5e7a5ed6d6a78c671b8aa347454845ff
Paulo Henrique <ph.costaalves@gmail.com>
2024-04-26 21:13:15 -0300

    testes e progressos no exercício 3

--- exerc3.c
+++ exerc3.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void strlen(char texto){
-  int i, qntdCarac;
-  for (i = 0; i < 60; i++){
-	if (texto[i] != "\0"){
-		qntdCarac++;
-	}
+int charTexto(char texto[]){
+  int tamanho = 0;
+  while (texto[tamanho] != '\0'){
+	tamanho++;
   }
-  printf("O texto: %s tem %i caracteres.", texto, qntdCarac);
-  return;
+  return tamanho;
 }
+
 
 int main (void){
 
@@ -19,7 +17,9 @@ int main (void){
   printf("Digite uma palavra: ");
   scanf("%s", texto[60]);
 
-  strlen(texto);
+  int tamanho = charTexto(texto);
+
+  printf("O texto: %s tem %i caracteres.", texto, tamanho);
 
   return 0;
 }
